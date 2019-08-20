e225b3a7302d4e54870f082e74536b8d73109ac6
khoa <martinchuanhkhoa@gmail.com>
2019-08-21 01:11:55 +0700

    kk

--- app/src/main/java/k8C/mqtt/MainActivity.java
+++ app/src/main/java/k8C/mqtt/MainActivity.java
@@ -75,48 +75,6 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
-public class MainActivity extends AppCompatActivity {
-    static final String TAG = "mqtt";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.e(TAG, "activity onCreate");
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        if (Build.VERSION.SDK_INT >= 26) { // create notification channel for android Oreo and above
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(new NotificationChannel("service", "Background Service", NotificationManager.IMPORTANCE_LOW));
-            notificationManager.createNotificationChannel(new NotificationChannel("mqttTopic", "MQTT Topics", NotificationManager.IMPORTANCE_HIGH));
-        }
-
-        ((ViewPager) findViewById(R.id.viewPager)).setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
-            @Override
-            public Fragment getItem(int position) {
-                return position == 0 ? new MqttFragment() : new DriveFragment();
-            }
-
-            @Override
-            public int getCount() {
-                return 2;
-            }
-
-            @Override
-            public CharSequence getPageTitle(int position) {
-                return position == 0 ? "mqtt" : "drive";
-            }
-        });
-    }
-
-    @Override // Activity Recreated after configuration changed
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        Log.e(TAG, "activity onRestoreInstanceState");
-        super.onRestoreInstanceState(savedInstanceState);
-    }
-
-}
 //https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.file&response_type=code&redirect_uri=urn:ietf:wg:oauth:2.0:oob&client_id=463875113005-1att4hu76j0ac7ta17mdjniinfgg2di2.apps.googleusercontent.com
 //1/0gh8dGi_1bkC-g5BFGlvRRJHKFwUYjQMpW59nv56ZVA
 
